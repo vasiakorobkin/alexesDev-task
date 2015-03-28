66509598ee3e187ce11f8b41f396d61e88756a5f
Vasia Korobkin <vasyakorobkin@taxasoftware.com>
2015-03-28 11:06:59 +0300

    View of child categories

--- src/client.js
+++ src/client.js
@@ -5,14 +5,16 @@ var Route = require('tuxx/Router/Route');
 var DefaultRoute = require('tuxx/Router/DefaultRoute');
 var App = require('./components/App.jsx');
 var CategoriesList = require('./components/CategoriesList.jsx');
+var CategoryInner = require('./components/CategoryInner.jsx');
 
 var routes = (
   <Route handler={App}>
     <DefaultRoute name='catalog' handler={CategoriesList} />
+    <Route path='/category/:id' handler={CategoryInner} />
   </Route>
 );
 
 run(routes, HistoryLocation, function(Handler, state) {
   var params = state.params;
-  React.render(<Handler />, document.getElementById('app'));
+  React.render(<Handler params={params}/>, document.getElementById('app'));
 });
---
+++ src/components/CategoryInner.jsx
@@ -0,0 +1,46 @@
+var React = require('tuxx/React');
+var CategoriesList = require('./CategoriesList.jsx');
+var categoriesStore = require('../stores/categoriesStore');
+var categoriesActions = require('../actions/categoriesActions');
+
+var CategoryInner = React.createOwnerClass({
+  getInitialState: function(){
+    categoriesActions.getAll();
+    return {
+      categories: categoriesStore.returnAll(),
+    }
+  },
+  connectOwnerToStore: function(){
+    return {
+      store: categoriesStore,
+      listener: function(){
+        this.setState({ categories: categoriesStore.returnAll() });
+      }.bind(this)
+    }
+  },
+  registerOwnerProps: function(){},
+  render: function(){
+    var id = this.props.params.id;
+    var categories = this.state.categories;
+    var current = categories.filter(function(category){
+      return category.id == id;
+    });
+    if(current.length == 0) return (
+      <div>Не удалось найти категорию</div>
+    ); 
+    if(current.length > 1) return (
+      <div>Ошибка: идентификатор категории не уникален</div>
+    );
+    var children = categories.filter(function(category){
+      return category.parentId == id
+    });
+    if(children.length > 0) return (
+      <CategoriesList parentId={id} />
+    );
+    else return (
+      <div>Список продуктов</div>
+    )
+  }
+});
+
+module.exports = CategoryInner;
